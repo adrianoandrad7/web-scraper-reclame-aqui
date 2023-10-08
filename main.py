93a81357a68722b145fbac85e469416d10ee077a
adriano andrade <62573290+adrianoandrad7@users.noreply.github.com>
2023-10-08 17:29:47 -0300

    melhorias no main e atualizacao do requirements

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from bs4 import BeautifulSoup
 import funcoes
 
 # Configurando o cabeçalho da requisição
-url_base = f'https://www.reclameaqui.com.br/empresa/unimed-bh/lista-reclamacoes/?busca=idoso&pagina='
+url_base = f'https://www.reclameaqui.com.br/empresa/unimed-bh/lista-reclamacoes/?busca=velhice&pagina='
 
-# Buscando o número de páginas
-pag_max = funcoes.coleta_pag_max(url_base + f'{1}')
+#Buscando o número de páginas
+#pag_max = funcoes.coleta_pag_max(url_base + f'{1}')
+
+pag_max = 1; 
 
 # Processando as páginas
 funcoes.processar_paginas(pag_max,url_base)
